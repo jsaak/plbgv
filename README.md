152e616644a245ddc386571178b9f04ee1174f69
jsaak <jsaak@napalm.hu>
2016-04-19 21:05:20 +0200

    raw numbers are ready

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 http://benchmarksgame.alioth.debian.org/u64q/summarydata.php
 http://gnuplot.sourceforge.net/demo_4.3/candlesticks.html
 http://gnuplot-tricks.blogspot.hu/2009/10/turning-of-histogram.html
+https://gist.github.com/Antti/5773468
--- graph.rb
+++ graph.rb
@@ -4,6 +4,36 @@
 require 'gnuplot'
 require 'pp'
 
+def median_split(arr)
+  c = arr.size%2
+  subset = [arr[0..arr.size/2-1], arr[arr.size/2+c..-1]]
+  m = median arr
+  subset[0].push m
+  subset[1].unshift m
+  subset
+end
+
+def median(arr)
+  if arr.size.odd?
+    arr[arr.size/2]
+  else
+    (arr[arr.size/2 -1 ].to_f + arr[arr.size/2].to_f) / 2
+  end
+end
+
+def quartile(arr,n=3)
+  arr.sort!
+  split = median_split arr
+  case n
+  when 1
+    median split[0]
+  when 2
+    median arr
+  when 3
+    median split[1]
+  end
+end
+
 data = File.readlines("clean-data.csv")
 
 #drop first line
@@ -18,19 +48,18 @@ multiple_data = multiple_data.each_slice(3).map(&:last)
 
 data2 = single_data + multiple_data
 
-Measurement = Struct.new(:time, :code_size, :mem_usage)
+Measurement = Struct.new(:time, :code_size, :mem)
 
 # get gcc results for normalizing
 tasks = Hash.new
 data2.each do |line|
   task,language,id,n,gz,cpu,kb,status,load_string,secs = line.strip.split(/,/)
-  pp language
   next unless language == 'C gcc'
   tasks[task] = Measurement.new(secs.to_f, gz.to_f, kb.to_f)
 end
 
 # update mem usage on meteor-contest, not to divide by 0
-tasks['meteor-contest'].mem_usage = 0.0001
+tasks['meteor-contest'].mem = 0.0001
 
 hash = Hash.new
 data2.each do |line|
@@ -39,16 +68,33 @@ data2.each do |line|
     m = Measurement.new
     m.time = Array.new
     m.code_size = Array.new
-    m.mem_usage = Array.new
+    m.mem = Array.new
     hash[language] = m
   end
   #normalize with gcc results
   hash[language].time.push     (secs.to_f / tasks[task].time)
   hash[language].code_size.push(gz.to_f / tasks[task].code_size)
-  hash[language].mem_usage.push(kb.to_f / tasks[task].mem_usage)
+  hash[language].mem.push(kb.to_f / tasks[task].mem)
 end
 
-pp hash
+Stat = Struct.new(:lang, :min, :q1, :median, :q3, :max)
+
+stat_time = Array.new
+stat_mem = Array.new
+stat_code = Array.new
+
+hash.each do |lang, m|
+  a = m.time.sort
+  stat_time.push(Stat.new(lang, a.min, quartile(a,1), quartile(a,2), quartile(a,3), a.max))
+  a = m.code_size.sort
+  stat_code.push(Stat.new(lang, a.min, quartile(a,1), quartile(a,2), quartile(a,3), a.max))
+  a = m.mem.sort
+  stat_mem.push(Stat.new(lang, a.min, quartile(a,1), quartile(a,2), quartile(a,3), a.max))
+end
+
+pp stat_time.sort_by(&:median)
+stat_mem.sort_by(&:median)
+stat_code.sort_by(&:median)
 
 
 exit
@@ -61,7 +107,7 @@ Gnuplot.open do |gp|
       # font = '/usr/share/fonts/truetype/windowsbol/tahomabd.ttf'
       # font = '/usr/share/fonts/truetype/msttcorefonts/Courier_New_Bold.ttf'
       plot.terminal("png medium size 1000,600 x000000 x777777 x404040 xff0000 xffa500 x66cdaa xcdb5cd xadd8e6 x0000ff xdda0dd x9500d3 font \"#{font}\" 9")
-      plot.output("poker.png") 
+      plot.output("game.png") 
 
       plot.key("left top")
       plot.grid("ytics")
